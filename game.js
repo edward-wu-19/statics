51aa71e013f47b2b35a61c421a1d58bf8d8d036a
LyfeMaster <64602623+LyfeMaster@users.noreply.github.com>
2022-05-27 11:14:13 -0400

    Stuff

--- game.js
+++ game.js
@@ -1,1 +1,28 @@
+// Task 1: Generate level
 
+// Task A: Generate canvas
+
+// Task B: Generate particle
+
+// Task C: Generate random forces
+
+//------------------------------------------------------------------------------
+// Task 2: Read Mouse
+
+// Task A: Read Mouse at Start, During, and Release
+
+// Task B: Calculate Mouse movement
+
+// Task C: Draw force vector
+
+//------------------------------------------------------------------------------
+// Task 3: Calculate Net Force
+
+// Task A: Convert each force from polar form into unit vector
+
+// Task B: Sum all forces
+
+// Task C: Determine the net force vector in polar form
+
+//------------------------------------------------------------------------------
+// Task 4: Animation 
