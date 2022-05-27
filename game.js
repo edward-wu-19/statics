9d276f641ea288309d2150c3a531c338930fb52f
LyfeMaster <64602623+LyfeMaster@users.noreply.github.com>
2022-05-27 11:42:04 -0400

    Laid out plans

--- game.js
+++ game.js
@@ -26,3 +26,13 @@
 
 //------------------------------------------------------------------------------
 // Task 4: Animation 
+
+// Task A: Calculate force and acceleration (Assume mass is a constant)
+// Technically, no scale for distance is provided so we just care about the shape of the x vs t graph
+
+// Task B: Use acceleration to calculate velocity
+// For simplicity, we could also just use the net force vector as the velocity vector and just say acceleration is 0 because friction is 0
+
+// Task C: Use velocity to calculate position
+
+// Task D: Simulate motion using play() from sample.js
