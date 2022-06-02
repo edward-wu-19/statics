8addc3c98b087ace1914a29f2917f6632e02b58c
Aaron Visser <avisser20@stuy.edu>
2022-06-02 11:42:05 -0400

    changes

--- scripts/level.js
+++ scripts/level.js
@@ -4,7 +4,7 @@ const particleTexture = PIXI.utils.TextureCache["res/circle.png"];
 function generateParticle(){
 
     // particle = new Sprite(particleTexture);
-    
+
     // particle.x = 128;
     // particle.y = 128;
 
@@ -20,10 +20,25 @@ function generateParticle(){
 
 generateParticle();
 
-function generateForce(){
+function generateForce(xmag, ymag, xpos, ypos){
+  let force = {x_mag: xmag, y_mag: ymag, x_pos: xpos, y_pos: ypos};
+  return force;
 }
 
-function createForces(n){
+function createForces(n, radius){
+  let force_array = {};
+  for(let i = 0; i < n; i++){
+    let theta = 2*Math.pi()*Math.random();
+    let xpos = radius * Math.cos(theta);
+    let ypos = radius * Math.sin(theta);
+    xmag = Math.random()*10;
+    ymag = Math.random()*10;
+    force_array.push(generateForce(xmag, ymag, xpos, ypos);
+  }
+  return a;
+}
+
+function netForce(force_array){
 
 }
 
